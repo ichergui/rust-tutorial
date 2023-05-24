cd55fe2f4c396bcfcd3e694cd02ea77d893bad90
Ilies CHERGUI <ilies.chergui@gmail.com>
2023-05-24 21:53:14 +0100

    add example-02 for basics
    Signed-off-by: Ilies CHERGUI <ilies.chergui@gmail.com>

---
+++ example-02/main.rs
@@ -0,0 +1,10 @@
+fn main () {
+    for index in 0..10 {
+        if index % 2 == 0 {
+            println!("index {} is even", index);
+        }
+        else {
+            println!("index {} is odd", index);
+        }
+    }
+}
